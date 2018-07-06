f6e2f3989cb94a5c57c59e72fe34babba550bb3f
Nauana Lima <nauanalima@id.uff.br>
2018-07-06 10:14:26 -0300

    Adição de funções pivoteamento e troca de linhas

--- pivoteamento.c
+++ pivoteamento.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 double subsreversa (double **matrix, double *root, const int dim) {
 	int j, k, n;
@@ -15,17 +16,57 @@ double subsreversa (double **matrix, double *root, const int dim) {
 	}
 }
 
-double triangsup (double **matrix, int dim) {
-	int i, j, k;
-	double aux;
-	for(j=0; j<=dim; j++) {
-		for (i=j+1; i<dim; i++) {
-			aux = matrix[i][j]/matrix[j][j];
-			for (k=0; k<dim+1; k++)
-				matrix[i][k] = matrix[i][k] - aux*matrix[j][k];		
-		}
+void imprime(double **matrix, int dim){
+	int i, j;
+	printf("Matriz: \n");
+	for(i=0;i<dim;i++) {
+		for(j=0;j<dim+1;j++) { 
+			printf("%5.2lf\t",matrix[i][j]);
+		}	  
+		printf("\n");
 	}
-	return **matrix;
+	printf("\n");
+}
+
+void trocar_linhas (double *line1, double *line2, int dim) {
+	double temp;
+	int i;
+
+	for (i=0; i<=dim; i++) {
+		temp = line1[i];
+		line1[i] = line2[i];
+		line2[i] = temp;
+	}
+}
+
+double **pivoteamento (double **matrix, int dim) {
+	int i, j, k, test;
+	double w, m, temp;
+
+	for (k=0; k<dim; k++) {
+		printf("\n\n------------ k = %d ---------------\n", k);
+		test = -1;
+		imprime(matrix,dim);
+		w = matrix[k][k];
+		for (j=k; j<dim; j++) {
+			if (fabs(matrix[j][k]) > w)
+					test = j;
+		}
+		if (test != -1) {
+			trocar_linhas(matrix[k], matrix[test], dim);
+			printf("\nMatriz após troca de linhas %d e %d: \n", k, test);
+			imprime(matrix,dim);
+		}
+		for (i=k+1; i<dim; i++) {
+			m = matrix[i][k]/matrix[k][k];
+			for (j=k; j<=dim; j++)
+				matrix[i][j] = matrix[i][j] - m*matrix[k][j];
+		}
+		printf("\nEstágio da matriz: \n");
+		imprime(matrix,dim);
+	}
+	printf("\n\n ---------------- fim do pivoteamento ------------------\n\n");
+	return matrix;		
 }
 
 double **ler(char *archive, int *dimension) {
@@ -47,4 +88,20 @@ double **ler(char *archive, int *dimension) {
 	}
 	*dimension = dim;
 	return matrix;
+}
+
+int main(int argc, char **argv) {
+	double **M;
+	double *raizes;
+	int i, dim;
+
+	M=ler(argv[1], &dim);
+	imprime(M, dim);
+	pivoteamento(M,dim);
+	imprime(M, dim);
+	raizes = malloc(dim*sizeof(double));
+	subsreversa(M,raizes,dim);
+	for (i=0; i<dim; i++)
+		printf("x%1d = %5.2lf\n", i, raizes[i]);
+	return 0;
 }
